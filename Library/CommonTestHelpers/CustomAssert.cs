edd6cb90e0319c919625143c26999cc996fdd4fa
Alex McNair <alex.mcnair@hotmail.co.uk>
2018-03-28 20:30:33 +0100

    fixed missing references

--- Library/CommonTestHelpers/CustomAssert.cs
+++ Library/CommonTestHelpers/CustomAssert.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Library.CommonTestHelpers
 {
